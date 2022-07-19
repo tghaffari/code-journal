9a193689865d52fab626748c1a7b45800f7d671b
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-19 16:55:18 +0000

    Updated issue-1-create-an-entry files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 
 var $img = document.querySelector('#img');
 var $photoUrl = document.querySelector('#photo-url');
@@ -12,13 +11,11 @@ $photoUrl.addEventListener('input', updateImage);
 
 function saveNewEntry(event) {
   event.preventDefault();
-  console.log('event.target', event.target.value);
   var titleInput = $newEntryForm.elements.title.value;
   var photoUrlInput = $newEntryForm.elements.url.value;
   var notesInput = $newEntryForm.elements.notes.value;
   var newEntryInput = { title: titleInput, photoUrl: photoUrlInput, notes: notesInput };
   newEntryInput.entryId = data.nextEntryId;
-  console.log('newEntryInput:', newEntryInput);
   data.nextEntryId++;
   data.entries.unshift(newEntryInput);
   $img.setAttribute('src', 'images/placeholder-image-square.jpg');
