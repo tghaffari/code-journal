d1e4d184a46b6d5e6c57bb3ad7e91598af1b01cc
Tarra Ghaffari <tarrag2525@gmail.com>
2022-07-19 21:51:39 +0000

    Updated nav bar for entries page for issue-2-user-can-view-their-entries.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,58 @@ function saveNewEntry(event) {
 }
 
 $newEntryForm.addEventListener('submit', saveNewEntry);
+
+function entryDomTreeCreation(entry) {
+
+  // <li class="bottom-margin">
+  //  <div class="row">
+  //    <div class="column-full column-half center">
+  //      <img class="img-placeholder bottom-margin" src=$photoURL //(value)>
+  //    </div>
+  //    <div class="column-full column-half">
+  //      <h1 class="title-styling">Title</h1>
+  //      <p class="notes-styling">Notes</p>
+  //    </div>
+  //  </div>
+  // </li>
+
+  var liElement = document.createElement('li');
+  liElement.setAttribute('class', 'bottom-margin');
+
+  var rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+  liElement.appendChild(rowDiv);
+
+  var imgColumnDiv = document.createElement('div');
+  imgColumnDiv.setAttribute('class', 'column-full column-half center');
+  rowDiv.appendChild(imgColumnDiv);
+
+  var imgElement = document.createElement('img');
+  imgElement.setAttribute('class', 'img-placeholder bottom-margin');
+  imgElement.setAttribute('src', entry.photoUrl);
+  imgColumnDiv.appendChild(imgElement);
+
+  var columnDiv = document.createElement('div');
+  columnDiv.setAttribute('class', 'column-full column-half');
+  rowDiv.appendChild(columnDiv);
+
+  var titleHeader = document.createElement('h1');
+  titleHeader.setAttribute('class', 'title-styling');
+  titleHeader.textContent = entry.title;
+  columnDiv.appendChild(titleHeader);
+
+  var notesParagraph = document.createElement('p');
+  notesParagraph.setAttribute('class', 'notes-styling');
+  notesParagraph.textContent = entry.notes;
+  columnDiv.appendChild(notesParagraph);
+
+  return liElement;
+}
+
+var $ul = document.querySelector('ul');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var newJournalEntry = entryDomTreeCreation(data.entries[i]);
+  $ul.appendChild(newJournalEntry);
+  document.addEventListener('DOMContentLoaded', entryDomTreeCreation);
+}
